58bc052dc482f2f75a6585e01fba2d9f053772eb
LAPTOP-4V0UT6U6\Acer <gundash1@gmail.com>
2026-06-14 10:56:21 +0700

    init employee domain

---
+++ employee/src/test/java/com/payroll_of_employee/employee/EmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payroll_of_employee.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
