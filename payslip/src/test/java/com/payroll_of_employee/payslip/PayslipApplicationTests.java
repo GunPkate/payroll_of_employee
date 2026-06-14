4321d9596584f6ab152cbba9200d5fca87f7fafa
LAPTOP-4V0UT6U6\Acer <gundash1@gmail.com>
2026-06-14 10:08:21 +0700

    init payslip domain

---
+++ payslip/src/test/java/com/payroll_of_employee/payslip/PayslipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payroll_of_employee.payslip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayslipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
